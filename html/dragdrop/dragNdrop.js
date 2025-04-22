9781dd740b1d4abd1239362c2aa84e1ecbeedeab
Your Name <you@example.com>
2025-04-23 00:58:22 +0900

    [drag-drop]複数の領域に対応

--- html/dragdrop/dragNdrop.js
+++ html/dragdrop/dragNdrop.js
@@ -1,48 +1,47 @@
 $(document).ready(function () {
-  const $dropArea = $('#drop-area');
-  const $fileInput = $('#fileInput');
-  const $fileList = $('#file-list');
+  const dropAreas = $('.drop-area');
 
-  // ファイル情報出力
-  function showFileInfo(file) {
-    $fileList.text(`ファイル名: ${file.name}`);
+  dropAreas.each(function() {
+    const $dropArea = $(this);
+    const fileInput = $dropArea.find('.file-input')[0];
+    const fileListId = $dropArea.data('file-list-id');
+    const $fileList = $('#' + fileListId);
+
+    // ドラッグオーバー処理
+    $dropArea.on('dragover', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $dropArea.addClass('hover');
+    });
+
+    $dropArea.on('dragleave', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $dropArea.removeClass('hover');
+    });
+
+    // ドロップ処理
+    $dropArea.on('drop', function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      $dropArea.removeClass('hover');
+
+      const files = e.originalEvent.dataTransfer.files;
+      handleFiles(files, $fileList);
+    });
+
+    fileInput.addEventListener('change', function() {
+      const files = this.files;
+      handleFiles(files, $fileList);
+    });
+  });
+
+  function handleFiles(files, $fileList) {
+    $fileList.empty();
+    for (let i = 0; i < files.length; i++) {
+      const file = files[i];
+      const listItem = file.name;
+      $fileList.prepend(listItem);
+    }
   }
-
-  // ファイル選択ボタン
-  $('#fileButton').on('click', function () {
-    $fileInput.click();
-  });
-
-  // ドラッグオーバー処理
-  $dropArea.on('dragover', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $dropArea.addClass('hover');
-  });
-
-  $dropArea.on('dragleave', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $dropArea.removeClass('hover');
-  });
-
-  // ドロップ処理
-  $dropArea.on('drop', function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-    $dropArea.removeClass('hover');
-
-    const files = e.originalEvent.dataTransfer.files;
-    if (files.length > 0) {
-      showFileInfo(files[0]);
-    }
-  });
-
-  // input ファイル選択処理
-  $fileInput.on('change', function (e) {
-    const file = e.target.files[0];
-    if (file) {
-      showFileInfo(file);
-    }
-  });
-});
+})
